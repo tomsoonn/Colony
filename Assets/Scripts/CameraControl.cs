514f08781da7b93ed5f4128405cb0506672408b0
ostro <ostr00000@gmail.com>
2019-05-06 23:13:36 +0200

    add images, screen scrol

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,16 +7,56 @@ public class CameraControl : MonoBehaviour
 {
     public float moveFactor = 0.5f;
     public float fasterFactor = 2f;
-    public Vector2 leftUpperBound = new Vector2Int(-100, -100);
-    public Vector2 rightBottomBound = new Vector2Int(100, 100);
+    public Vector2 leftUpperBound = new Vector2Int(-20, -20);
+    public Vector2 rightBottomBound = new Vector2Int(20, 20);
+
+    public float minZoom = 3f;
+    public float maxZoom = 15f;
+    private Camera _camera;
 
     private void Start()
     {
+        _camera = Camera.main;
         Assert.Less(leftUpperBound.x, rightBottomBound.x, "incorrect bound values set");
         Assert.Less(leftUpperBound.y, rightBottomBound.y, "incorrect bound values set");
     }
 
     private void Update()
+    {
+        MoveCamera();
+        ZoomCamera();
+    }
+
+    private void ZoomCamera()
+    {
+        var zoom = _camera.orthographicSize - Input.GetAxis("Mouse ScrollWheel") * 10f;
+        float newFov;
+        if (zoom < minZoom)
+        {
+            newFov = minZoom;
+        }
+        else if (zoom > maxZoom)
+        {
+            newFov = maxZoom;
+        }
+        else
+        {
+            newFov = zoom;
+        }
+
+        _camera.orthographicSize = newFov;
+    }
+
+    private float Clamp(float value, float min, float max)
+    {
+        if ((double) value < (double) min)
+            value = min;
+        else if ((double) value > (double) max)
+            value = max;
+        return value;
+    }
+
+    private void MoveCamera()
     {
         var deltaX = Input.GetAxisRaw("Horizontal") * moveFactor;
         var deltaY = Input.GetAxisRaw("Vertical") * moveFactor;
