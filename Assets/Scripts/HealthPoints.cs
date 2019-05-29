e32548d80947bddffa9debb820b1d25795032ad0
ostro <ostr00000@gmail.com>
2019-05-29 13:55:58 +0200

    enemy hp, warrior attack first neighborly enemy

---
+++ Assets/Scripts/HealthPoints.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPoints : MonoBehaviour
+{
+    public int MaxHealth = 10;
+    public int Health = 10;
+
+    void Update()
+    {
+        if (Health < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Units/Warrior.cs
+++ Assets/Scripts/Units/Warrior.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class Warrior : Unit
 {
+    public float DeltaDistance = 3f;
+    public int AttackDamage = 1;
+    public float AttackTime=0.5f;
 
-    public int attack = 2;
     // Start is called before the first frame update
     void Start()
     {
         name = "Warrior";
+        StartCoroutine("Attack");
+    }
+
+    private IEnumerator Attack()
+    {
+        for (;;)
+        {
+            yield return new WaitForSeconds(AttackTime);
+            var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (var enemy in enemies)
+            {
+                if (Vector3.Distance(enemy.transform.position, gameObject.transform.position) < DeltaDistance)
+                {
+                    enemy.GetComponent<HealthPoints>().Health -= 1;
+                    Debug.Log("Attacking");
+                    break;
+                }
+            }
+        }
     }
 }
